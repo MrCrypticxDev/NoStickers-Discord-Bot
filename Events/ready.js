7d216bcd3163e7cb7d9f4ec1a818e5a47a8786c4
Justin <justinleeong@gmail.com>
2021-06-10 13:48:30 +0800

    Fix the ready event
    * Use chalk for logging errors from the .catch statement.
    * Use spaces in objects.

--- Events/ready.js
+++ Events/ready.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 
 module.exports = (client) => {
   // eslint-disable-next-line max-len
-  client.user.setActivity(`Deleting Stickers | !help`, {type: 'PLAYING'}).catch(console.error);
+  client.user.setActivity(`Deleting Stickers | !help`, { type: 'PLAYING' }).catch((e) => console.log(chalk.red(e)));
 
   console.log(chalk.yellow(`${client.user.username} is online!`));
 };
