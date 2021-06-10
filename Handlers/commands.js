d491f6bf29f64fd0439167856cc32448d5e6ed1f
Justin <justinleeong@gmail.com>
2021-06-10 13:47:00 +0800

    Update the command handler
    * Use chalk for logging errors.
    * Fix the require statements.
    * Use Error instead of ReferenceError (this isn't applicable in the situation).
    * Fix wording.
    * Remove extra whitespace.

--- Handlers/commands.js
+++ Handlers/commands.js
@@ -1,5 +1,6 @@
-const path = require('path');
+const chalk = require('chalk');
 const getFiles = require('../Structures/Utils/getFiles.js');
+const path = require('path');
 
 module.exports = (client) => {
   getFiles('./Commands').filter((str) => str.endsWith('.js')).forEach((fp) => {
@@ -8,19 +9,18 @@ module.exports = (client) => {
       const cmd = new Prop();
       cmd.category = path.dirname(fp).split(path.sep).reverse()[0];
       // eslint-disable-next-line max-len
-      if (!cmd.name) throw new ReferenceError(`${path.basename(fp)} does not have a name property!`);
+      if (!cmd.name) throw new Error(`${path.basename(fp)} does not have a name property!`);
       client.commands.set(cmd.name, cmd);
       if (!cmd.aliases) return;
       // eslint-disable-next-line max-len
-      if (!Array.isArray(cmd.aliases)) throw new TypeError(`${path.basename(fp)} aliases aren't array datatype`);
+      if (!Array.isArray(cmd.aliases)) throw new TypeError(`${path.basename(fp)} aliases provided aren't arrays.`);
       cmd.aliases.forEach((alias) => {
         client.aliases.set(alias, cmd);
       });
     } catch (e) {
       // eslint-disable-next-line max-len
-      if (e.message == 'prop is not a constructor') return console.log(`${fp} cannot create a instaniated class`);
-      console.log(e);
+      if (e.message == 'prop is not a constructor') return console.log(`${fp} cannot create a new class instance.`);
+      console.log(chalk.red(e));
     }
   });
 };
-
