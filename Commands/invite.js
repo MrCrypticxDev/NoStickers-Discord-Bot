5df2a29ea2e9057ec6d4010b344763b4f930e830
Justin <justinleeong@gmail.com>
2021-06-10 13:25:30 +0800

    Update the invite command structure
    * Prefer returns over the return tag.
    * Use Array<string>.
    * Fix wording.
    * Remove excess whitespace on top.

--- Commands/invite.js
+++ Commands/invite.js
@@ -1,21 +1,20 @@
-
 /**
  * Invite command
  */
 class inviteCommand {
   /**
-   * Assign command properties
+   * Assign the command properties
    */
   constructor() {
     this.name = 'invite';
   }
 
   /**
-   * invite link to invite bot
+   * The link for inviting the bot
    * @param {client} client discord.js client instance
    * @param {message} message discord.js message instance
-   * @param {Array.<string>} args message arguments
-   * @return {void}
+   * @param {Array<string>} args message arguments
+   * @returns {void}
    */
   async main(client, message, args) {
     return message.channel.send(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=-9`);
