1e529f5bd98d309f2a6fd572a5b5fd6c8d2f473f
Justin <justinleeong@gmail.com>
2021-06-10 13:43:16 +0800

    Update the event handler
    * Use chalk for logging errors.
    * Implement try/catch.
    * Fix the stack of require statements.

--- Handlers/events.js
+++ Handlers/events.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const chalk = require('chalk');
 const getFiles = require('../Structures/Utils/getFiles.js');
+const path = require('path');
 
 module.exports = (client) => {
   const items = getFiles(`./Events`);
   items.forEach((filepath) => {
-    // try{
-    const eName = path.basename(filepath).replace('.js', '');
-    const evt = require(`.${filepath}`);
-    client.on(eName, evt.bind(null, client));
-    // }catch(e){}
+    try {
+      const eName = path.basename(filepath).replace('.js', '');
+      const evt = require(`.${filepath}`);
+      client.on(eName, evt.bind(null, client));
+    } catch(e) {
+      console.log(chalk.red(e));
+    }
   });
 };
