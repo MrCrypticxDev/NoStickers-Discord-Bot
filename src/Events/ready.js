bead7b7e61153fa804e6036ef2a5a793f4ca577d
Justin <justinleeong@gmail.com>
2021-06-12 13:53:44 +0800

    Update ready event structure and details
    * Remove the ESLint ignore line.
    * Use the specified prefix instead of "!".

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,8 +1,7 @@
 const chalk = require('chalk');
 
 module.exports = (client) => {
-  // eslint-disable-next-line max-len
-  client.user.setActivity('Deleting Stickers | !help', { type: 'PLAYING' }).catch((e) => console.log(chalk.red(e)));
+  client.user.setActivity(`Deleting Stickers | ${client.config.prefix} help`, { type: 'PLAYING' }).catch((e) => console.log(chalk.red(e)));
 
   console.log(chalk.yellow(`${client.user.username} is online!`));
 };
