453459deb46de9aa6403d35ea976a3dbb0a18a52
MrCrypticXDev <Info@aspirerbx.com>
2021-06-18 09:58:43 -0400

    Removed the extra ',' at the mongodb field.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicKey: 'YOUR_APP_PUBLIC_KEY',
   prefix: 'YOUR_BOT_PREFIX',
   token: 'YOUR_BOT_TOKEN',
-  mongoURL: ',YOUR_MONGODB_URL',
+  mongoURL: 'YOUR_MONGODB_URL',
   ownerIDs: [
     'YOUR_USER_ID'
   ]
